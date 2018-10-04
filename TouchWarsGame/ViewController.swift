0660eedfe1dce77daec36df0e2de935374fd7a76
Kub <kub@Kub-MacBook-Pro.local>
2018-10-04 17:59:09 +0300

    ButtonClick race

--- TouchWarsGame/ViewController.swift
+++ TouchWarsGame/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var player1score:Int = 0
+    var player2score:Int=0
+    var sonuc:String=""
+    var totalscore:Int=10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func btnPlayer1(_ sender: Any) {
+        player1score=player1score+1
+        player2score=player2score-1
+        if(player1score==totalscore){
+            self.sonuc="Player 1 kazandı"
+          performSegue(withIdentifier: "sonuc", sender: self)
+        }
+        
 
+    }
+    
+    @IBAction func btnPlayer2(_ sender: Any) {
+        player1score=player1score-1
+        player2score=player2score+1
+        if(player2score==totalscore){
+            self.sonuc="Player 2 kazandı"
+            performSegue(withIdentifier: "sonuc", sender: self)
+        }
+        
+    }
+  
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.destination is SecondViewController
+        {
+            let vc = segue.destination as? SecondViewController
+            vc?.sonucc=sonuc
+        }
+    }
 
+    
 }
 
